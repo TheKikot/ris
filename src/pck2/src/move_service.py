f3b860e39a94626e2915360889d97317b3451e26
Krištof Brezar <kb5710@student.uni-lj.si>
2019-03-13 00:49:43 +0100

    completed the service, testing todo

--- src/pck2/src/move_service.py
+++ src/pck2/src/move_service.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
+import random
 import rospy
 from pck2.srv import movesrv
 from geometry_msgs.msg import Twist
 
+last_shape = "none"
+speed = 5
+
+def random_movement(step):
+	
+	twist = Twist()
+	twist.linear.x = random.uniform(0.0,2.0)*speed
+	twist.angular.z = random.uniform(-3.14,3.14)
+
+	return twist
+
 def rectangle_movement(step):
 
   twist = Twist()
-  twist.linear.x = 0.1
+  twist.linear.x = 0.1*speed
   step = step % 20
 
   if step % 5 == 0:
@@ -19,7 +31,7 @@ def rectangle_movement(step):
 def triangle_movement(step):
 
   twist = Twist()
-  twist.linear.x = 0.1
+  twist.linear.x = 0.1*speed
   step = step % 15
 
   if step % 5 == 0:
@@ -31,16 +43,48 @@ def triangle_movement(step):
 def circle_movement(step):
 
   twist = Twist()
-  twist.linear.x = 0.02
-  twist.angular.z = 0,017	#(1 / 360) * 2 * 3.14
-  step = step % 20
+  twist.linear.x = 0.3*speed
+  twist.angular.z = 0.3*speed	#(1 / 360) * 2 * 3.14
 
   return twist
+  
+def movement(shape, time):
+	pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 1000)
+	# For the turtle simulation map the topic to /turtle1/cmd_vel
+	# For the turtlebot simulation and Turtlebot map the topic to /cmd_vel_mux/input/navi
+	#rospy.init_node('movement')
+	
+	# we will be changing the last_shape variable
+	global last_shape
+	
+	# assign the right function
+	if shape == 'rectangle':
+		move_fun = rectangle_movement
+	elif shape == 'triangle':
+		move_fun = triangle_movement
+	elif shape == 'circle':
+		move_fun = circle_movement
+	else:
+		move_fun = random_movement
+
+	freq = 1
+	r = rospy.Rate(freq)
+
+	step = 0.0
+
+	while step <= time*freq:
+		twist = move_fun(step)
+		step = step + 1.0
+		pub.publish(twist)
+		r.sleep()
+		
+	last_shape = shape
 
 def move_server_function(req):
-    print("Shape: " + req.shape)
+    #print("Shape: " + req.shape)
     #print("TIME: " + (str)req.time)
-    return "7"
+    movement(req.shape, req.time)
+    return last_shape
 
 def move_service():
     rospy.init_node('move_service')
