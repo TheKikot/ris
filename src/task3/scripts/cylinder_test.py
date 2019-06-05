9d69cb806c4cd6eed4e881992309740a26c0cdf5
Mihael <ms0555@student.uni-lj.si>
2019-06-05 16:23:35 +0200

    nedelujoč service za call_cylinder_detection

--- src/task3/scripts/cylinder_test.py
+++ src/task3/scripts/cylinder_test.py
@@ -1,49 +1,16 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-#ni nč trenutno
+
 import sys
 import rospy
 from task3.srv import *
-
-def get_location_client():
-    # get image
-		rospy.wait_for_service('get_ring_location')
-		try:
-				get_cylinder_location = rospy.ServiceProxy('get_cylinder_location', GetLocation)
-				get_cylinder_location()
-		except rospy.ServiceException, e:
-				print "Service call failed: %s"%e
-
-if __name__ == "__main__":
-    get_location_client()
-    print ""
-=======
-
-import rospy
-#import python-pcl
-from sensor_msgs.msg import PointCloud2
-import sensor_msgs.point_cloud2 as pc2
-
-def call_detection(points):
-	points_pub = rospy.Publisher("input", PointCloud2, queue_size=1)
-	print("publishing points")
-	points_pub.publish(points)	
-
-
-
-
+from std_msgs.msg import String
 
 def main():
-	print("running cylinder test")
-	rospy.init_node('cylinder_detection', anonymous=False)
-	
-	points_sub = rospy.Subscriber("/camera/depth_registered/points", PointCloud2, call_detection)
-	print("Shutting down")
+    # get image
+	rospy.init_node('cylinder_test', anonymous=False)
+	points_pub = rospy.Publisher("/detect_cylinder", String, queue_size=1)
+	points_pub.publish("a")
 
-
-
-if __name__ == '__main__':
-	main()
->>>>>>> 9f88991baa5a9579ba8baed514f0109d58c703e7
-
-
+if __name__ == "__main__":
+    main()
+    print ""
