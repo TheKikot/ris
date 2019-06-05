6ba9c8c95cc3253f8e6e2e09279438bb3da6bbef
Krištof Brezar <kb5710@student.uni-lj.si>
2019-06-05 14:51:56 +0200

    merging

--- src/task3/scripts/cylinder_test.py
+++ src/task3/scripts/cylinder_test.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 #import python-pcl
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 
 def call_detection(points):
+	print("klicem funkcijo")
 	points_pub = rospy.Publisher("input", PointCloud2, queue_size=1)
-	print("publishing points")
 	points_pub.publish(points)	
+	sys.exit()
 
 
 
 
 
 def main():
-	print("running cylinder test")
+	print("setting up node")
 	rospy.init_node('cylinder_detection', anonymous=False)
 	
 	points_sub = rospy.Subscriber("/camera/depth_registered/points", PointCloud2, call_detection)
-	print("Shutting down")
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+	  print("Shutting down")
 
 
 
