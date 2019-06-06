44091b91c6282e455e3595d0332b99841e724fd3
Mihael <ms0555@student.uni-lj.si>
2019-06-06 02:39:04 +0200

    continous search for QR

--- src/task3/scripts/detect_rings_reformated.py
+++ src/task3/scripts/detect_rings_reformated.py
@@ -386,6 +386,10 @@ class The_Ring:
         self.normals_pub.publish(marker)
 
     def image_callback(self, krneki):
+        while(self.check_for_QR() == 0):
+            print('failure')
+
+        print('success!!')
         print('I got a new image!')
         data = rospy.wait_for_message('/camera/rgb/image_raw', Image)
 
@@ -670,6 +674,11 @@ class The_Ring:
                         print('The extracted datapoints are x=%d, y=%d' % (x,y))
                     else:
                         print('The extracted text has is of length %d. Aborting processing' % len(text))
+                        while self.check_for_QR() == 0:
+                            print('failure')
+
+                        print('success!!')
+                        '''
                         print("Looking for QR codes")
                         # Find a QR code in the image
                         decodedObjects = pyzbar.decode(img_out_QR)
@@ -702,10 +711,11 @@ class The_Ring:
                                     img_out_QR)
                                 
                         elif len(decodedObjects)==0:
-                            print("No QR code in the image")
+                            print("Havent found any QR codes")
                             #TODO mapa
                         else:
                             print("Found more than 1 QR code")
+                            '''
 
                     
                 else:
@@ -719,6 +729,80 @@ class The_Ring:
             print('Didnt find any circles. ')
 
         return []
+
+    def check_for_QR(self):
+        
+        data = rospy.wait_for_message('/camera/rgb/image_raw', Image)
+        print('Grabbed a new image')
+
+
+        try:
+            print('converting image')
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            print('image converted!')
+        except CvBridgeError, e:
+            print(e)
+
+        # 640x480
+
+        #cv_image = cv_image[80:420, 0:640]
+        cv_image_copy = cv_image.copy()
+
+        # Set the dimensions of the image
+
+        self.dims = cv_image.shape
+
+        # Tranform image to gayscale
+
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+
+        # Do histogram equlization
+
+        img = cv2.equalizeHist(gray)
+
+
+        # Binarize the image
+        # ret, thresh = cv2.threshold(img, 50, 255, 0)
+
+        thresh = cv2.adaptiveThreshold(
+            img,
+            255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY,
+            151,
+            2,
+            )
+
+        kernel = np.ones((3, 3), np.uint8)
+
+        #thresh = cv2.dilate(thresh, kernel, iterations = 1)
+        #cv2.waitKey(0)
+
+
+        
+        print("Looking for QR codes")
+        # Find a QR code in the image
+        decodedObjects = pyzbar.decode(thresh)
+        cv2.imwrite('cv_image_copy.png',
+                    cv_image_copy)
+        
+        #print(decodedObjects)
+        
+        if len(decodedObjects) == 1:
+            dObject = decodedObjects[0]
+            print("Found 1 QR code in the image!")
+            print("Data: ", dObject.data,'\n')
+            return dObject.data;        
+
+                
+        elif len(decodedObjects)==0:
+            print("Havent found any QR codes")
+            return 0;
+            #TODO mapa
+        else:
+            print("Found more than 1 QR code")
+            return 0;
 
     def depth_callback(self, data):
 
