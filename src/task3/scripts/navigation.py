cf9b0930db8fb9c47a355e61766ef7e6555fda05
Krištof Brezar <kb5710@student.uni-lj.si>
2019-06-07 12:50:13 +0200

    obrezan zemljevid, delo na nav

--- src/task3/scripts/navigation.py
+++ src/task3/scripts/navigation.py
@@ -11,7 +11,7 @@ import numpy as np
 import tf2_geometry_msgs
 import tf2_ros
 from nav_msgs.srv import GetMap
-from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 #from sensor_msgs.msg import Image
 #from geometry_msgs.msg import PointStamped, Vector3, Pose
 #from cv_bridge import CvBridge, CvBridgeError
@@ -19,16 +19,21 @@ from nav_msgs.msg import OccupancyGrid
 #from std_msgs.msg import ColorRGBA
 #from task3.srv import *
 
-def read_map(staticMap):
-	print(len(staticMap)/ 512)
+def read_map(mapData):
+	meta = mapData.info
+	staticMap = mapData.data
+	# dimenzije : 75 x 100
+	for i in range(0,100):
+		print(staticMap[i*75+0:i*75+75])
+	
 
 def main():
 	rospy.init_node('navigation', anonymous=False)
 	rospy.wait_for_service('static_map')
 	try:
 		mapService = rospy.ServiceProxy('static_map', GetMap)
-		staticMap = mapService().map.data
-		read_map(staticMap)
+		mapData = mapService().map
+		read_map(mapData)
 	except Exception, e:
 		print(e)
 
