4d0cd303e026eabec8ef865bf578579fed7911af
Mihael <ms0555@student.uni-lj.si>
2019-06-09 18:29:04 +0200

    popravki v cylinder segmentation

--- src/task3/src/cylinder_segmentation.cpp
+++ src/task3/src/cylinder_segmentation.cpp
@@ -61,20 +61,10 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   pcl::fromPCLPointCloud2 (*cloud_blob, *cloud);
   std::cerr << "PointCloud has: " << cloud->points.size () << " data points." << std::endl;
 
-  geometry_msgs::PointStamped point_camera;
-	point_camera.header.frame_id = "camera_rgb_optical_frame";
-	point_camera.header.stamp = ros::Time::now();
-
-	  point_camera.point.x = centroid[0];
-	  point_camera.point.y = centroid[1];
-	  point_camera.point.z = centroid[2];
-
-
-
   // Build a passthrough filter to remove spurious NaNs
   pass.setInputCloud (cloud);
   pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0, 2.3);
+  pass.setFilterLimits (0, 2.5);
   pass.filter (*cloud_filtered);
   std::cerr << "PointCloud after filtering z has " << cloud_filtered->points.size () << " data points." << std::endl;
 
@@ -86,7 +76,7 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   
 	// Reduce load by downsampling the cloud
 	vox_grid.setInputCloud (cloud_filtered);
-	vox_grid.setLeafSize (0.01f, 0.01f, 0.01f);
+	vox_grid.setLeafSize (0.02f, 0.02f, 0.02f);
   vox_grid.filter(*cloud_filtered);
   
   std::cerr << "PointCloud after downsampling with leaf size " << vox_grid.getLeafSize() << " has " << cloud_filtered->points.size () << " data points." << std::endl;
@@ -108,7 +98,7 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   seg.setInputNormals (cloud_normals);
   // Obtain the plane inliers and coefficients
   seg.segment (*inliers_plane, *coefficients_plane);
-  std::cerr << "Plane coefficients: " << *coefficients_plane << std::endl;
+  //std::cerr << "Plane coefficients: " << *coefficients_plane << std::endl;
 
   // Extract the planar inliers from the input cloud
   extract.setInputCloud (cloud_filtered);
@@ -125,9 +115,9 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   	std::cerr << "plane: " << cloud_plane->points[i].y << std::endl;
   	*/
 
-  pcl::PCLPointCloud2 outcloud_plane;
+  /*pcl::PCLPointCloud2 outcloud_plane;
   pcl::toPCLPointCloud2 (*cloud_plane, outcloud_plane);
-  pubx.publish (outcloud_plane);
+  pubx.publish (outcloud_plane);*/
 
   // Remove the planar inliers, extract the rest
   extract.setNegative (true);
@@ -144,12 +134,12 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   seg.setNormalDistanceWeight (0.1);
   seg.setMaxIterations (10000);
   seg.setDistanceThreshold (0.05);
-  seg.setRadiusLimits (0.10, 0.15);
+  seg.setRadiusLimits (0.11, 0.14);
   seg.setInputCloud (cloud_filtered2);
   seg.setInputNormals (cloud_normals2);
 
 
-	std::cerr << "Looking for cylinders with a radius between 0.1 and 0.15 m" << std::endl;
+	std::cerr << "Looking for cylinders with a radius between 0.11 and 0.14 m" << std::endl;
   // Obtain the cylinder inliers and coefficients
   seg.segment (*inliers_cylinder, *coefficients_cylinder);
   // std::cerr << "Cylinder coefficients: " << *coefficients_cylinder << std::endl;
@@ -165,44 +155,81 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
   else
   {
 	  std::cerr << "PointCloud representing the cylindrical component: " << cloud_cylinder->points.size () << " data points." << std::endl;
-    /*      
-    for (int i = 0; i < cloud_cylinder->points.size(); i+= 20)
-		std::cerr << "cylinder: " << cloud_cylinder->points[i].y << std::endl;
-	*/
-
-    pcl::compute3DCentroid (*cloud_cylinder, centroid);
-    //std::cerr << "centroid of the cylindrical component: " << centroid[0] << " " <<  centroid[1] << " " <<   centroid[2] << " " <<   centroid[3] << std::endl;
+          
+          pcl::compute3DCentroid (*cloud_cylinder, centroid);
+          //std::cerr << "centroid of the cylindrical component: " << centroid[0] << " " <<  centroid[1] << " " <<   centroid[2] << " " <<   centroid[3] << std::endl;
 
 	  //Create a point in the "camera_rgb_optical_frame"
-    geometry_msgs::PointStamped point_map;
-	  point_map.header.frame_id = "map";
-point_map.header.stamp = ros::Time::now();
-    visualization_msgs::Marker marker;
-    geometry_msgs::TransformStamped tss;
+          geometry_msgs::PointStamped point_camera;
+          geometry_msgs::PointStamped point_map;
+	      visualization_msgs::Marker marker;
+          geometry_msgs::TransformStamped tss;
           
-    
-	  try
-	  {
+          point_camera.header.frame_id = "camera_rgb_optical_frame";
+          point_camera.header.stamp = ros::Time::now();
+
+	  	  point_map.header.frame_id = "map";
+          point_map.header.stamp = ros::Time::now();
+
+		  point_camera.point.x = centroid[0];
+		  point_camera.point.y = centroid[1];
+		  point_camera.point.z = centroid[2];
+
+	  try{
 		  time_test = ros::Time::now();
 
-		  std::cerr << time_rec << std::endl;
-		  std::cerr << time_test << std::endl;
-      tss = tf2_buffer.lookupTransform("map","camera_rgb_optical_frame", time_rec);
-      //tf2_buffer.transform(point_camera, point_map, "map", ros::Duration(2));
+		  //std::cerr << time_rec << std::endl;
+		  //std::cerr << time_test << std::endl;
+  	      tss = tf2_buffer.lookupTransform("map","camera_rgb_optical_frame", time_rec);
+          //tf2_buffer.transform(point_camera, point_map, "map", ros::Duration(2));
 	  }
-    catch (tf2::TransformException &ex)
+          catch (tf2::TransformException &ex)
 	  {
-			ROS_WARN("Transform warning: %s\n", ex.what());
+	       ROS_WARN("Transform warning: %s\n", ex.what());
 	  }
 
           //std::cerr << tss ;
-          
-    tf2::doTransform(point_camera, point_map, tss);
 
-    std::cerr << "point_camera: " << point_camera.point.x << " " <<  point_camera.point.y << " " <<  point_camera.point.z << std::endl;
+          tf2::doTransform(point_camera, point_map, tss);
 
-    std::cerr << "point_map: " << point_map.point.x << " " <<  point_map.point.y << " " <<  point_map.point.z << std::endl;
-	      
+	      //std::cerr << "point_camera: " << point_camera.point.x << " " <<  point_camera.point.y << " " <<  point_camera.point.z << std::endl;
+
+	      //std::cerr << "point_map: " << point_map.point.x << " " <<  point_map.point.y << " " <<  point_map.point.z << std::endl;
+
+	  	  marker.header.frame_id = "map";
+          marker.header.stamp = ros::Time::now();
+
+          marker.ns = "cylinder";
+          marker.id = 0;
+
+          marker.type = visualization_msgs::Marker::CYLINDER;
+          marker.action = visualization_msgs::Marker::ADD;
+
+          marker.pose.position.x = point_map.point.x;
+          marker.pose.position.y = point_map.point.y;
+          marker.pose.position.z = point_map.point.z;
+          marker.pose.orientation.x = 0.0;
+	      marker.pose.orientation.y = 0.0;
+          marker.pose.orientation.z = 0.0;
+          marker.pose.orientation.w = 1.0;
+
+          marker.scale.x = 0.2;
+	      marker.scale.y = 0.2;
+	      marker.scale.z = 0.2;
+
+          marker.color.r=0.0f;
+          marker.color.g=0.0f;
+          marker.color.b=0.0f;
+          marker.color.a=1.0f;
+
+	      marker.lifetime = ros::Duration();
+
+	      pubm.publish (marker);
+
+	      pcl::PCLPointCloud2 outcloud_cylinder;
+          pcl::toPCLPointCloud2 (*cloud_cylinder, outcloud_cylinder);
+          puby.publish (outcloud_cylinder);
+
     // call color service
     task3::GetColor message;
     message.request.cam_X = point_camera.point.x;
@@ -216,47 +243,11 @@ point_map.header.stamp = ros::Time::now();
     if(serv.call(message))
     {
     	color_code = message.response.color;
-		}
-		else
-		{
-			std::cerr << "service error" << std::endl;
-		}
-		
-		
-	  marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    marker.ns = "cylinder";
-    marker.id = 0;
-
-    marker.type = visualization_msgs::Marker::CYLINDER;
-    marker.action = visualization_msgs::Marker::ADD;
-
-    marker.pose.position.x = point_map.point.x;
-    marker.pose.position.y = point_map.point.y;
-    marker.pose.position.z = point_map.point.z;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
-
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0f;
-
-    marker.lifetime = ros::Duration();
-
-    pubm.publish(marker);
-    
-
-    pcl::PCLPointCloud2 outcloud_cylinder;
-    pcl::toPCLPointCloud2 (*cloud_cylinder, outcloud_cylinder);
-    puby.publish(outcloud_cylinder);
+	}
+	else
+	{
+		std::cerr << "service error" << std::endl;
+	}
 
   }
   
